adc9b8a383ce65ee04cb95cff18724a85c6dc056
Florian <me@florian.ws>
2020-10-09 19:23:33 +0200

    Add routes to test server; add yarn.lock

--- example-server/src/app.js
+++ example-server/src/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.header("origin"));
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE")
   res.header("Access-Control-Allow-Credentials", "true")
-})
+  next();
+});
 
-app.get('/', (req, res) => {
-  res.send({
-    test: 1
-  })
+["get", "post", "put", "patch", "delete"].forEach(method => {
+  app[method](`/okay`, (req, res) => res.send({status: "success", date: new Date().toString()}));
+  app[method](`/error`, (req, res) => res.send({status: "error", date: new Date().toString()}));
 })
 
 app.listen(3001, () => {
